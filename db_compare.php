f3d7617644e7744981d90e9a8319606029c0151f
Andrew <andymc@inbox.ru>
2024-09-24 21:00:37 +0300

    db_compare

--- db_compare.php
+++ db_compare.php
@@ -14,157 +14,50 @@ if (!defined('DIR_MYSQL')) {
 }
 $msc->pageTitle = 'Сравнение баз данных';
 
-
-
-//$databases = ['home1c', 'symfony_home1c'];
-
-
 // Проверка
-$databases = POST('databases');
-if (count($databases) < 2) {
+$databases = explode(';', POST('dbs'));
+if (!$databases) {
+    $databases = POST('databases');
+}
+if ($databases && count($databases) < 2) {
     $msc->addMessage('Вы не выбрали базы данных для сравнения');
     return null;
 }
 
-/*
-$pageProps = [
-    'databases' => $databases
-];
+function pageProps($databases) {
+    global $msc;
+
+    // Создание начальных массивов
+    $dbArray = array();
+    foreach ($databases as $k => $v) {
+        $result = $msc->query('SHOW TABLE STATUS FROM '.$v);
+        while ($row = mysqli_fetch_object($result)) {
+            $dbArray[$v][$row->Name]= $row;
+        }
+    }
+
+    $exportArray = [];
+    $export = new MySQLExport();
+    $export->setComments(0);
+    $export->setOptionsStruct(0, $addAuto=0, 0);
+    foreach ($dbArray as $db => $tables) {
+        foreach ($tables as $table => $values) {
+            $export->data = null;
+            $export->setDatabase($db);
+            $export->setTable($table);
+            $exportData = $export->exportStructure(0,0);
+            $exportData = str_replace(' PACK_KEYS=0', '', $exportData);
+            $exportData = preg_replace('~COMMENT=".*"~U', '', $exportData);
+            $exportArray [$db][$table] = $exportData;
+        }
+    }
+    return compact('databases', 'dbArray', 'exportArray');
+}
+
+
+$pageProps = pageProps($databases);
+//echo '<pre>'; print_r($pageProps); echo '</pre>'; exit;
 if (isajax()) {
     return $pageProps;
 }
 include(MS_DIR_TPL . 'db_compare.htm.php');
-*/
-
-// Создание начальных массивов
-$dbArray = array();
-$dbSimpleArray = array();
-foreach ($databases as $k => $v) {
-    $result = $msc->query('SHOW TABLE STATUS FROM '.$v);
-    $dbArray[$k] = array();
-    $dbSimpleArray[$v] = array();
-    while ($row = mysqli_fetch_object($result)) {
-        $dbArray[$v][$row->Name]= $row;
-        $dbSimpleArray[$v][]= $row->Name;
-    }
-}
-// Созание полного списка таблиц двух БД
-$tablesArray = array();
-foreach ($dbSimpleArray as $v) {
-  $tablesArray = array_merge($tablesArray, $v);
-}
-$tablesArray = array_unique($tablesArray);
-asort($tablesArray);
-
-// ГЛАВНАЯ ТАБЛИЦА
-
-// Шапка таблицы
-$params = array('Есть?', 'Рядов', 'Размер', 'Стр-ра');
-$customHeader = '<tr><td rowspan=2>&nbsp;</td><td rowspan=2>Таблица</td>';
-$dbc = count($databases);
-foreach ($params as $param) {
-    $customHeader .= '
-    <td colspan="'.$dbc.'">'.$param.'</td>
-    ';
-}
-$customHeader .= '</tr><tr>';
-foreach ($params as $param) {
-    foreach ($databases as $v) {
-        $customHeader .= '<td>'.$v.'</td>
-    ';
-    }
-}
-$customHeader .= '</tr>';
-
-// Старт создания главной таблицы
-$export = new MySQLExport();
-$export->setComments(0);
-$export->setOptionsStruct(0, $addAuto=0, 0);
-$exportDifference = array();
-$tableObject = new Table('contentTable anone');
-$tableObject->setHeadContent($customHeader);
-$tableObject->setColClass('', 'font-weight:bold','b','b','r','r','r','r','r','r','r');
-foreach ($tablesArray as $tableNum => $table) {
-    $row = array();
-    $row []= '<input name="table[]" type="checkbox" value="'.$table.'" class="cb" id="t-'.$tableNum.'">';
-    $row []= '<label for="t-'.$tableNum.'">'.$table.'</a>';
-    $attributes = array('', '');
-    $rowStyle = null;
-    $skip = false;
-    foreach ($params as $num => $param) {
-        $countRowsPrev  = null;
-        $valueSizePrev  = null;
-        $exportDataPrev = null;
-        foreach ($dbSimpleArray as $db => $array) {
-            $export->data = null;
-            $attr = null;
-            if ($num == 0) {
-                if (in_array($table, $array)) {
-                    $row []= '<a href="?db='.$db.'&table='.$table.'&s=tbl_data">есть</a>';
-                } else {
-                    $row []= '-';
-                    $rowStyle = ' class="diff"';
-                }
-            }
-            if ($num == 1) {
-                $countRows = isset($dbArray[$db][$table]) ? $dbArray[$db][$table]->Rows : '-';
-                if ($countRows === $countRowsPrev) {
-                    $attr = ' style="background-color:#ccffcc"';
-                    $attributes [count($attributes)-1] = $attr;
-                }
-                $row []= $countRows;
-                $countRowsPrev = $countRows;
-            }
-            if ($num == 2) {
-                if (isset($dbArray[$db][$table])) {
-                    $valueData = $dbArray[$db][$table]->Data_length + $dbArray[$db][$table]->Index_length;
-                    $valueData = MSC_roundZero($valueData / 1024, 2);
-                    if ($valueData === $valueSizePrev) {
-                        $attr = ' style="background-color:#99ff99"';
-                        $attributes [count($attributes)-1] = $attr;
-                        //$skip = true;
-                    }
-                    $row []= $valueData;
-                    $valueSizePrev = $valueData;
-                } else {
-                    $row []= '-';
-                }
-            }
-            if ($num == 3) {
-                if (isset($dbArray[$db][$table])) {
-                    $export->setDatabase($db);
-                    $export->setTable($table);
-                    $exportData = $export->exportStructure(0,0);
-                    $exportData = str_replace(' PACK_KEYS=0', '', $exportData);
-                    $exportData = preg_replace('~COMMENT=".*"~U', '', $exportData);
-                    if ($exportData == $exportDataPrev) {
-                        $attr = ' style="background-color:#66ff66"';
-                        $attributes [count($attributes)-1] = $attr;
-                        //$skip = true;
-                    } elseif ($exportDataPrev != null) {
-                        $exportDifference[$table] = array($exportDataPrev, $exportData);
-                    }
-                    $exportDataPrev = $exportData;
-                    $row []= '+';
-                } else {
-                    $row []= '-';
-                }
-            }
-            $attributes [] = $attr;
-        }
-    }
-    if ($skip) {
-        continue;
-    }
-    $tableObject->makeRow($row, $rowStyle, false, $attributes);
-}
-
-?>
-
-<form method="post" action="?s=tbl_compare">
-    <input type="hidden" name="tableComparsion" value="1">
-    <input type="hidden" name="databases" value="<?php echo implode(',', $databases)?>">
-    <input tabindex="100" type="submit" value="Сравнить выбранные" class="submit" />
-    <?php echo $tableObject->make()?>
-</form>
-<?php echo pre($exportDifference)?>
--- tpl/db_compare.htm.php
+++ tpl/db_compare.htm.php
@@ -1,6 +1,3 @@
-
-
-
 <div id="root"></div>
 
 <script type="text/babel">
@@ -12,82 +9,161 @@
       this.state = {data: false}
     }
 
-    async loadAll() {
-      let sql, mode, a;
-      let data = {};
-      for (let database of this.props.databases) {
-        sql = `SHOW TABLE STATUS FROM ${database}`;
-        mode = 'querysql'
-        a = await querySql({sql, mode}, 'json')
-        data [database] = a;
+    header(params) {
+      let cellsWithTitles = []
+      let dbc = this.props.databases.length;
+      for (let param of params) {
+        cellsWithTitles.push(<td key={param} colSpan={dbc}>{param}</td>)
       }
-      this.setState({data})
+      let cellsWithDatabases = []
+      for (let param of params) {
+        for (let v of this.props.databases) {
+          cellsWithDatabases.push(<td key={param + v}>{v}</td>)
+        }
+      }
+      return <React.Fragment>
+        <tr>
+          <td rowSpan="2">&nbsp;</td>
+          <td rowSpan="2">Таблица</td>
+          {cellsWithTitles}</tr>
+        <tr>{cellsWithDatabases}</tr>
+      </React.Fragment>
     }
 
-    componentDidMount () {
-      this.loadAll()
+    getTablesArray(dbArray) {
+      let tablesArray = {}
+      for (const db in dbArray) {
+        for (const table in dbArray[db]) {
+          tablesArray [table]= table;
+        }
+      }
+      tablesArray = Object.values(tablesArray)
+      tablesArray.sort()
+      return tablesArray
+    }
+
+    body(params) {
+      let dbArray = this.props.dbArray;
+      let exportArray = this.props.exportArray;
+      const tablesArray = this.getTablesArray(dbArray)
+      const exportDifference = {}
+      let rows = []
+      for (let tableNum = 0; tableNum < tablesArray.length; tableNum++) {
+        const table = tablesArray[tableNum]
+        let row = []
+        row.push(<input name="table[]" type="checkbox" value={table} className="cb" id={`t-${tableNum}`}/>)
+        row.push(<label htmlFor={`t-${tableNum}`}>{table}</label>)
+        let rowClass = '';
+        let skip = false
+        for (let num = 0; num < params.length; num++) {
+          const param = params[num]
+          let countRowsPrev = null;
+          let valueSizePrev = null;
+          let exportDataPrev = null;
+          for (const db in dbArray) {
+            const array = Object.keys(dbArray[db])
+            let attr = {} // cell style
+            // Существует или нет - просто
+            if (num === 0) {
+                if (array.includes(table)) {
+                  row.push(<a href={`/tbl_data/${db}/${table}`}>есть</a>)
+                } else {
+                  row.push('-')
+                  rowClass = 'diff';
+                }
+            }
+            // Сравнение по количеству строк
+            if (num === 1) {
+              const countRows = dbArray[db][table] ? dbArray[db][table].Rows : '-';
+              if (countRows === countRowsPrev) {
+                attr.backgroundColor = '#ccffcc';
+              }
+              row.push({text: countRows, style: attr})
+              countRowsPrev = countRows;
+            }
+            // Сравнение по размеру данных
+            if (num === 2) {
+              if (dbArray[db][table]) {
+                let valueData = parseInt(dbArray[db][table].Data_length) + parseInt(dbArray[db][table].Index_length);
+                valueData = formatSize(valueData, 0);
+                if (valueData === valueSizePrev) {
+                  attr.backgroundColor = '#99ff99';
+                }
+                row.push({text: valueData, style: attr})
+                valueSizePrev = valueData;
+              } else {
+                row.push('-')
+              }
+            }
+            // Сравнение структур из экспорта
+            if (num === 3) {
+              if (dbArray[db][table]) {
+                let exportData = exportArray[db][table]
+                if (exportData) {
+                    if (exportData === exportDataPrev) {
+                      attr.backgroundColor = '#66ff66';
+                    } else if (exportDataPrev !== null) {
+                      exportDifference [table] = [exportDataPrev, exportData]
+                    }
+                }
+                exportDataPrev = exportData;
+                row.push({text: '+', style: attr})
+              } else {
+                row.push('-')
+              }
+            }
+          }
+        }
+
+        rows.push(this.buildRow(row, rowClass, rows.length))
+      }
+
+      return {rows, exportDifference}
+    }
+
+    printDifference(exportDifference) {
+      let values = []
+      for (const db in exportDifference) {
+        for (const table in exportDifference[db]) {
+          let data = exportDifference[db][table]
+          values.push(<li>{db} {table} <pre>{data}</pre></li>)
+        }
+      }
+      return <ul>{values}</ul>
+    }
+
+    buildRow(values, rowClass, rowIndex) {
+      let cells = []
+      values.forEach(function (value, key) {
+        let style = {}
+        if (typeof value.style !== 'undefined') {
+          style = value.style
+          value = value.text
+        }
+        cells.push(<td key={`td-${rowIndex}-${key}`} style={style}>{value}</td>)
+      })
+      return <tr key={`row-${rowIndex}`} className={rowClass}>{cells}</tr>
     }
 
     render() {
-      if (!this.state.data) {
-        return false;
-      }
-
-      let dbArray = {}
-      let dbSimpleArray = {}
-      let tablesArray = {}
-      let data;
-      let k = 0;
-      for (let database of this.props.databases) {
-        data = this.state.data[database]
-        if (typeof data == 'undefined') {
-          continue;
-        }
-        dbArray [k] = {}
-        dbSimpleArray [database] = []
-        for (let row of data) {
-          if (typeof dbArray [database] == 'undefined') {
-            dbArray [database] = []
-          }
-          dbArray [database][row.Name] = row;
-          dbSimpleArray [database].push(row.Name)
-          tablesArray [row.Name] = row.Name;
-        }
-        k ++
-      }
-
-      tablesArray = Object.values(tablesArray)
-      tablesArray.sort()
-
       let params = ['Есть?', 'Рядов', 'Размер', 'Стр-ра']
-
-      let tds = []
-      let dbc = this.props.databases.count;
-      for (let param of params) {
-        tds.push(<td key={param} colSpan={dbc}>{param}</td>)
-      }
-      let tds2 = []
-      for (let param of params) {
-        for (let v of this.props.databases) {
-          tds2.push(<td key={param+v}>{v}</td>)
-        }
-      }
-      let customHeader = <React.Fragment>
-        <tr><td rowSpan="2">&nbsp;</td><td rowSpan="2">Таблица</td>{tds}</tr>
-        <tr>{tds2}</tr>
-      </React.Fragment>
-
-      //console.log(tablesArray)
-      //console.log(dbSimpleArray)
-
+      const customHeader = this.header(params)
+      const {rows, exportDifference} = this.body(params)
       return (
-        <table className="contentTable anone">
-          <thead>
-          {customHeader}
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
+        <form method="post" action="/tbl_compare">
+          <input type="hidden" name="tableComparsion" value="1"/>
+          <input type="hidden" name="databases" value={this.props.databases.join(',')}/>
+          <input type="submit" value="Сравнить выбранные" className="submit"/>
+          <table className="contentTable anone">
+            <thead>
+            {customHeader}
+            </thead>
+            <tbody>
+            {rows}
+            </tbody>
+          </table>
+          {this.printDifference(exportDifference)}
+        </form>
       );
     }
   }
